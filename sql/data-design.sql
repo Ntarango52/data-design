c2e090c29a93f652b0ebb7e1fc7b0c5e2ee38bc4
Nohemi Tarango <Nohemi5270@gmail.com>
2020-03-17 14:28:30 -0600

    made the changes cecilia told me to fix

--- sql/data-design.sql
+++ sql/data-design.sql
@@ -14,16 +14,17 @@ create table profile(
 	profileId BINARY(16) NOT NULL,
 	profileDate DATETIME(6) NOT NULL,
 	profileUserId BINARY(16) NOT NULL,
-	PRIMARY KEY(profileId)
+	PRIMARY KEY(profileId),
+	FOREIGN KEY(profileUserId) REFERENCES user(userId)
 );
 
-create table 'show'(
-	showUserId BINARY(16) NOT NULL,
-	showProfileId BINARY(16) NOT NULL,
-	showContent VARCHAR(4000),
-	INDEX(showUserId),
-	INDEX(showProfileId),
-	FOREIGN KEY(showUserId) REFERENCES user(userId),
-	FOREIGN KEY(showProfileId) REFERENCES profile(profileId),
-	PRIMARY KEY(showUserId, showProfileId)
+create table media(
+	mediaUserId BINARY(16) NOT NULL,
+	mediaProfileId BINARY(16) NOT NULL,
+	mediaContent VARCHAR(4000),
+	INDEX(mediaUserId),
+	INDEX(mediaProfileId),
+	FOREIGN KEY(mediaUserId) REFERENCES user(userId),
+	FOREIGN KEY(mediaProfileId) REFERENCES profile(profileId),
+	PRIMARY KEY(mediaUserId, mediaProfileId)
 );
